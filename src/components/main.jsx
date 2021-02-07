5c703ff537820495931279c9fc43e016e718bf43
vovapipko <vpmail13@gmail.com>
2021-02-07 15:00:35 +0300

    Опишет компоненты главной страницы приложения

---
+++ src/components/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../components/logo';
+import MovieCard from '../components/movie-card';
+import PageContent from '../components/page-content';
+
+const Main = () => {
+  return (
+    <Logo />,
+    <MovieCard />,
+    <PageContent />
+  );
+};
+
+export default Main;

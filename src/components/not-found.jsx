a078e9ce5c1c8b337c4b19c6da55fbb6dda9ab2f
vovapipko <vpmail13@gmail.com>
2021-02-13 17:31:12 +0300

    Создаст компонент-заглушку 404

---
+++ src/components/not-found.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+      </header>
+      <div className="sign-in user-page__content">
+        <h1 className="page-title user-page__title">
+          <span style={{display: `block`, fontSize: 100, marginBottom: 30}}>404</span>
+            Not Found
+        </h1>
+      </div>
+      <footer className="page-footer">
+        <div className="logo">
+          <a href="main.html" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+
+  );
+};
+
+export default NotFound;

f4163ec78b37f5e1faa4dd6a6229278a505d9649
vovapipko <vpmail13@gmail.com>
2021-02-20 19:17:34 +0300

    Подключит моки в App и добавит валидацию. WIP

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -10,12 +10,12 @@ import AddReviewScreen from '../add-review-screen/add-review-screen';
 import PlayerScreen from '../player-screen/player-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 
-const App = ({promoCard, miniCardData}) => {
+const App = ({films}) => {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <MainScreen promoCard={promoCard} miniCardData={miniCardData}/>
+          <MainScreen films={films}/>
         </Route>
         <Route exact path="/login">
           <SignInScreen />
@@ -41,22 +41,27 @@ const App = ({promoCard, miniCardData}) => {
 };
 
 App.propTypes = {
-  promoCard: PropTypes.shape({
-    keyname: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    genre: PropTypes.string.isRequired,
-    premiere: PropTypes.shape({
-      date: PropTypes.string,
-      year: PropTypes.string.isRequired,
-    }).isRequired,
-  }),
-  miniCardData: PropTypes.arrayOf(
+  films: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.number,
-        keyname: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-      }).isRequired,
-  ).isRequired,
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        posterImage: PropTypes.string.isRequired,
+        previewImage: PropTypes.string.isRequired,
+        backgroundImage: PropTypes.string.isRequired,
+        backgroundColor: PropTypes.string.isRequired,
+        videoLink: PropTypes.string.isRequired,
+        previewVideoLink: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        scoresCount: PropTypes.number.isRequired,
+        director: PropTypes.string.isRequired,
+        starring: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+        runTime: PropTypes.number.isRequired,
+        genre: PropTypes.string.isRequired,
+        released: PropTypes.number.isRequired,
+        isFavorite: PropTypes.bool.isRequired
+      }).isRequired
+  ).isRequired
 };
 
 export default App;

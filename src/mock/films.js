d42db1470bc50a863577dd35b48f04db863fade8
vovapipko <vpmail13@gmail.com>
2021-02-20 20:05:05 +0300

    Исправит src картинки в моках

--- src/mock/films.js
+++ src/mock/films.js
@@ -4,7 +4,7 @@ export const films = [
     name: `The Grand Budapest Hotel`,
     posterImage: `img/the-grand-budapest-hotel-poster.jpg`,
     previewImage: `img/the-grand-budapest-hotel.jpg`,
-    backgroundImage: `img/the-grand-budapest-hotel-bg.jpg`,
+    backgroundImage: `img/bg-the-grand-budapest-hotel.jpg`,
     backgroundColor: `#ffffff`,
     videoLink: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
     previewVideoLink: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,

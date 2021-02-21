208adbebe4cabd594e75e846234fd1827793f4c3
vovapipko <vpmail13@gmail.com>
2021-02-21 16:57:37 +0300

    Обновит компонент add-review-screen. WIP

--- src/components/add-review-screen/add-review-screen.jsx
+++ src/components/add-review-screen/add-review-screen.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import CommentForm from '../comment-form/comment-form';
 
-const AddReviewScreen = () => {
+const AddReviewScreen = ({films, id}) => {
+  const film = films[id - 1];
+  const {backgroundImage, backgroundColor, name, posterImage} = film;
+
   return (
-    <section className="movie-card movie-card--full">
+    <section className="movie-card movie-card--full" style={{backgroundColor}}>
       <div className="movie-card__header">
         <div className="movie-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+          <img src={backgroundImage} alt={name} />
         </div>
         <h1 className="visually-hidden">WTW</h1>
         <header className="page-header">
@@ -20,7 +25,7 @@ const AddReviewScreen = () => {
           <nav className="breadcrumbs">
             <ul className="breadcrumbs__list">
               <li className="breadcrumbs__item">
-                <a href="movie-page.html" className="breadcrumbs__link">The Grand Budapest Hotel</a>
+                <Link to={`/films/${id}`} className="breadcrumbs__link">{name}</Link>
               </li>
               <li className="breadcrumbs__item">
                 <a className="breadcrumbs__link">Add review</a>
@@ -34,46 +39,40 @@ const AddReviewScreen = () => {
           </div>
         </header>
         <div className="movie-card__poster movie-card__poster--small">
-          <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width={218} height={327} />
+          <img src={posterImage} alt={name} width={218} height={327} />
         </div>
       </div>
       <div className="add-review">
-        <form action="#" className="add-review__form">
-          <div className="rating">
-            <div className="rating__stars">
-              <input className="rating__input" id="star-1" type="radio" name="rating" defaultValue={1} />
-              <label className="rating__label" htmlFor="star-1">Rating 1</label>
-              <input className="rating__input" id="star-2" type="radio" name="rating" defaultValue={2} />
-              <label className="rating__label" htmlFor="star-2">Rating 2</label>
-              <input className="rating__input" id="star-3" type="radio" name="rating" defaultValue={3} defaultChecked />
-              <label className="rating__label" htmlFor="star-3">Rating 3</label>
-              <input className="rating__input" id="star-4" type="radio" name="rating" defaultValue={4} />
-              <label className="rating__label" htmlFor="star-4">Rating 4</label>
-              <input className="rating__input" id="star-5" type="radio" name="rating" defaultValue={5} />
-              <label className="rating__label" htmlFor="star-5">Rating 5</label>
-              <input className="rating__input" id="star-6" type="radio" name="rating" defaultValue={6} />
-              <label className="rating__label" htmlFor="star-6">Rating 6</label>
-              <input className="rating__input" id="star-7" type="radio" name="rating" defaultValue={7} />
-              <label className="rating__label" htmlFor="star-7">Rating 7</label>
-              <input className="rating__input" id="star-8" type="radio" name="rating" defaultValue={8} defaultChecked />
-              <label className="rating__label" htmlFor="star-8">Rating 8</label>
-              <input className="rating__input" id="star-9" type="radio" name="rating" defaultValue={9} />
-              <label className="rating__label" htmlFor="star-9">Rating 9</label>
-              <input className="rating__input" id="star-10" type="radio" name="rating" defaultValue={10} />
-              <label className="rating__label" htmlFor="star-10">Rating 10</label>
-            </div>
-          </div>
-          <div className="add-review__text">
-            <textarea className="add-review__textarea" name="review-text" id="review-text" placeholder="Review text" style={{marginTop: 0, marginBottom: 0, height: 150}} defaultValue={``} />
-            <div className="add-review__submit">
-              <button className="add-review__btn" type="submit">Post</button>
-            </div>
-          </div>
-        </form>
+        <CommentForm />
       </div>
     </section>
 
   );
+};
+
+AddReviewScreen.propTypes = {
+  films: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        posterImage: PropTypes.string.isRequired,
+        previewImage: PropTypes.string.isRequired,
+        backgroundImage: PropTypes.string.isRequired,
+        backgroundColor: PropTypes.string.isRequired,
+        videoLink: PropTypes.string.isRequired,
+        previewVideoLink: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        scoresCount: PropTypes.number.isRequired,
+        director: PropTypes.string.isRequired,
+        starring: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+        runTime: PropTypes.number.isRequired,
+        genre: PropTypes.string.isRequired,
+        released: PropTypes.number.isRequired,
+        isFavorite: PropTypes.bool.isRequired
+      }).isRequired
+  ).isRequired,
+  id: PropTypes.number,
 };
 
 export default AddReviewScreen;

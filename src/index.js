67ebcaf3214863e3cd7ded521a3b31366dc83f95
vovapipko <vpmail13@gmail.com>
2021-03-14 23:28:23 +0300

    Обновит index.js - импортирует thunk из redux-thunk - импортирует applyMiddleware из redux - импортирует createApi из файла api.js - создаст api и запишет в переменную - обновит код создания store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import App from './components/app/app';
 import {FilmsGenres, Lists} from './const';
 import {films} from './mock/films';
 import {getGenres} from './utils';
 import {reducer} from './store/reducer';
 import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import {createApi} from './api/api';
 
-const store = createStore(reducer);
+const api = createApi();
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk.withExtraArgument(api))
+);
+
 const filmsGenres = [FilmsGenres.DEFAULT, ...getGenres(films, Lists.MAX_GENER_TABS)];
 
 ReactDOM.render(

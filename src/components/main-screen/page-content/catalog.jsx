971e325fe9e593be27db9ea530809808612b29a5
vovapipko <vpmail13@gmail.com>
2021-03-16 00:23:46 +0300

    Рефакторинг компонета Catalog

--- src/components/main-screen/page-content/catalog.jsx
+++ src/components/main-screen/page-content/catalog.jsx
@@ -1,32 +1,39 @@
 import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import MoviesList from '../../movies-list/movies-list';
-import {propFilm} from '../../../props-validation';
 import {connect} from 'react-redux';
+
+import {propFilm} from '../../../props-validation';
 import {ActionCreator} from '../../../store/actions';
 import {FilmsGenres, Lists} from '../../../const';
 import {getFimlsByGenre, getGenres} from './../../../utils';
-import MoreButton from './more-button';
-import GenresTabs from './genres-tabs';
 
-const Catalog = (props) => {
-  const {state, onGenreChange} = props;
+import GenresTabs from './genres-tabs';
+import MoviesList from '../../movies-list/movies-list';
+import MoreButton from './more-button';
+
+const Catalog = ({state, onGenreChange}) => {
+  // TODO: из пропсов приходят фильмы.
+  // TODO: пока оставить. буду работать из состояния.
+  const {films, genre} = state;
 
   const [showFilmsCount, setShowFilmsCount] = useState(Lists.START_VIEWCARD);
-  // TODO: isMoreButtonVisible ??setIsShowButtonVisible??
   const [moreButtonVisible, setShowButtonVisible] = useState(false);
 
   useEffect(() => {
-    if (state.films.length > showFilmsCount) {
+    if (films.length > showFilmsCount) {
       setShowButtonVisible(true);
     } else {
       setShowButtonVisible(false);
     }
-  }, [showFilmsCount, state.genre]);
+  }, [showFilmsCount, genre]);
 
-  const handleGenresTabsClick = (evt, genre) => {
+
+  const genres = [FilmsGenres.DEFAULT, ...getGenres(films, Lists.MAX_GENER_TABS)];
+  const filmsByGenre = getFimlsByGenre(films, genre, FilmsGenres.DEFAULT).slice(0, showFilmsCount);
+
+  const handleGenresTabsClick = (evt, genreInState) => {
     evt.preventDefault();
-    onGenreChange(ActionCreator.showGenre(genre));
+    onGenreChange(ActionCreator.showGenre(genreInState));
     setShowFilmsCount(Lists.START_VIEWCARD);
   };
 
@@ -34,13 +41,10 @@ const Catalog = (props) => {
     setShowFilmsCount((count) => count + Lists.STEP_VIEWCARD);
   };
 
-  const genres = [FilmsGenres.DEFAULT, ...getGenres(state.films, Lists.MAX_GENER_TABS)];
-  const filmsByGenre = getFimlsByGenre(props.films, state.genre, FilmsGenres.DEFAULT).slice(0, showFilmsCount);
-
   return (
     <section className="catalog">
       <h2 className="catalog__title visually-hidden">Catalog</h2>
-      <GenresTabs genres={genres} genreInState={state.genre} onGenreTabClick={handleGenresTabsClick} />
+      <GenresTabs genres={genres} genreInState={genre} onGenreTabClick={handleGenresTabsClick} />
       <MoviesList films={filmsByGenre} />
       <MoreButton isVisible={moreButtonVisible} onShowMoreButtonClick={handleShowMoreButtonClick} />
     </section>
@@ -48,16 +52,17 @@ const Catalog = (props) => {
 };
 
 Catalog.propTypes = {
-  films: PropTypes.arrayOf(
-      PropTypes.shape(propFilm).isRequired
-  ).isRequired,
-  onGenreChange: PropTypes.func.isRequired,
+  // TODO: передаём фильмы в пропсах
+  // films: PropTypes.arrayOf(
+  //     PropTypes.shape(propFilm).isRequired
+  // ).isRequired,
   state: PropTypes.shape({
     genre: PropTypes.string.isRequired,
     films: PropTypes.arrayOf(
         PropTypes.shape(propFilm).isRequired
     ).isRequired,
   }).isRequired,
+  onGenreChange: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => {

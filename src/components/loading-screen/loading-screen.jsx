43afcbb42fcf4a9a7ee21d36b9420d6c38e8e321
vovapipko <vpmail13@gmail.com>
2021-03-17 01:35:37 +0300

    Fix. 10#discussion_r595422035 LoadingScreen - удалит футер из компонента

--- src/components/loading-screen/loading-screen.jsx
+++ src/components/loading-screen/loading-screen.jsx
@@ -22,18 +22,6 @@ const LoadingScreen = () => {
           <div className="lds-spinner"><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /><div /></div>
         </div>
       </div>
-      <footer className="page-footer">
-        <div className="logo">
-          <a to="/" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
     </div>
   );
 };

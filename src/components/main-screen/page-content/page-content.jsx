1631bda499daa7db668f02d981919756cd8423a5
vovapipko <vpmail13@gmail.com>
2021-03-17 01:43:54 +0300

    Fix. 10#discussion_r595422844 Компонент Footer - создаст dumb компонент Footer - подключит его в PageContent

--- src/components/main-screen/page-content/page-content.jsx
+++ src/components/main-screen/page-content/page-content.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'react-router-dom';
 
 import {propFilm} from '../../../props-validation';
 import Catalog from './catalog';
+import Footer from '../../footer/footer';
 
 const PageContent = ({films}) => {
 
   return (
     <div className="page-content">
       <Catalog films={films}/>
-      <footer className="page-footer">
-        <div className="logo">
-          <Link to="/" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </Link>
-        </div>
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };

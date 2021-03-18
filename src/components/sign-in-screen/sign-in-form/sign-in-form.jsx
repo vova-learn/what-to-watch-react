3b652495e8c66f4e3384856f2d69c376c8f0dfd1
vovapipko <vpmail13@gmail.com>
2021-03-18 20:14:10 +0300

    POST. Реализует авторизацию пользователя - обновит компонент SignInScreen - вынесет форму авторизации в отдельный компонент - в компоненте формы подпишется на глобавльный статус авторизации - подпишется на поля ввода - реализует отправку формы на сервер - обработает ошибку незаполненного пароля

---
+++ src/components/sign-in-screen/sign-in-form/sign-in-form.jsx
@@ -0,0 +1,79 @@
+import React, {createRef, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import Swal from 'sweetalert2';
+
+import browserHistory from '../../../browser-history';
+import {login} from '../../../store/api-actions';
+import {AuthorizationStatus, ErrorMessageText, RouteApp} from '../../../const';
+import {initErrorAlert} from '../../../utils';
+
+const SignInForm = ({authorizationStatus, onSubmit}) => {
+  useEffect(() => {
+    if (authorizationStatus === AuthorizationStatus.AUTH) {
+      /*
+        При переходе на закрытый маршрут по прямой ссылке
+        не получается вернуться назад навигацией браузера
+      */
+      if (browserHistory.location.state) {
+        browserHistory.push(browserHistory.location.state.prevPath);
+      } else {
+        browserHistory.push(RouteApp.MAIN);
+      }
+    }
+  }, [authorizationStatus]);
+
+  const emailInput = createRef();
+  const passwordInput = createRef();
+
+  const handleFromSubmit = (evt) => {
+    evt.preventDefault();
+
+    if (!passwordInput.current.value) {
+      initErrorAlert(Swal.fire, ErrorMessageText.PASSWORD_EMPTY);
+
+      return;
+    }
+
+    onSubmit(emailInput.current.value, passwordInput.current.value);
+  };
+
+  return (
+    <form action="#" className="sign-in__form" onSubmit={handleFromSubmit}>
+      <div className="sign-in__fields">
+        <div className="sign-in__field">
+          <input ref={emailInput} className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+          <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+        </div>
+        <div className="sign-in__field">
+          <input ref={passwordInput} className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+          <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+        </div>
+      </div>
+      <div className="sign-in__submit">
+        <button className="sign-in__btn" type="submit" >Sign in</button>
+      </div>
+    </form>
+  );
+};
+
+SignInForm.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    authorizationStatus: state.authorizationStatus,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSubmit(email, password) {
+      dispatch(login({login: email, password}));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignInForm);
--- src/components/sign-in-screen/sign-in-screen.jsx
+++ src/components/sign-in-screen/sign-in-screen.jsx
@@ -1,48 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+
+import Header from '../header/header';
+import SignInForm from './sign-in-form/sign-in-form';
+import Footer from '../footer/footer';
 
 const SignInScreen = () => {
   return (
     <div className="user-page">
-      <header className="page-header user-page__head">
-        <div className="logo">
-          <Link className="logo__link" to="/">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </Link>
-        </div>
+      <Header isUserBlock={false} >
         <h1 className="page-title user-page__title">Sign in</h1>
-      </header>
+      </Header>
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
-          <div className="sign-in__fields">
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
-            </div>
-            <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
-              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
-            </div>
-          </div>
-          <div className="sign-in__submit">
-            <button className="sign-in__btn" type="submit">Sign in</button>
-          </div>
-        </form>
+        <SignInForm />
       </div>
-      <footer className="page-footer">
-        <div className="logo">
-          <Link to="/" className="logo__link logo__link--light">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </Link>
-        </div>
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };

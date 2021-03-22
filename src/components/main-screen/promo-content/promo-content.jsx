e7da030af6c3fd9404488532262fa1555e1e4ed6
vovapipko <vpmail13@gmail.com>
2021-03-22 19:51:08 +0300

    Включит кнопку My list на главном экране

--- src/components/main-screen/promo-content/promo-content.jsx
+++ src/components/main-screen/promo-content/promo-content.jsx
@@ -19,7 +19,9 @@ const PromoContent = ({promoFilm}) => {
         <img src={backgroundImage} alt={name} />
       </div>
       <h1 className="visually-hidden">WTW</h1>
+
       <Header />
+
       <div className="movie-card__wrap">
         <div className="movie-card__info">
           <div className="movie-card__poster">
@@ -32,13 +34,23 @@ const PromoContent = ({promoFilm}) => {
               <span className="movie-card__year">{released}</span>
             </p>
             <div className="movie-card__buttons">
-              <button className="btn btn--play movie-card__button" type="button" onClick={() => history.push(`/player/${id}`)}>
+
+              <button
+                className="btn btn--play movie-card__button"
+                type="button"
+                onClick={() => history.push(`/player/${id}`)}
+              >
                 <svg viewBox="0 0 19 19" width={19} height={19}>
                   <use xlinkHref="#play-s" />
                 </svg>
                 <span>Play</span>
               </button>
-              <button className="btn btn--list movie-card__button" type="button">
+
+              <button
+                className="btn btn--list movie-card__button"
+                type="button"
+                onClick={() => history.push(`/mylist`)}
+              >
                 <svg viewBox="0 0 19 20" width={19} height={20}>
                   <use xlinkHref="#add" />
                 </svg>

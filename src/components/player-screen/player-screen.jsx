bf7cd79d8bf8b98427c476990a30317b4659e309
vovapipko <vpmail13@gmail.com>
2021-03-24 23:44:19 +0300

    VideoPlayer 1.3 Запустит работу плеера

--- src/components/player-screen/player-screen.jsx
+++ src/components/player-screen/player-screen.jsx
@@ -1,18 +1,26 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {useHistory} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import {fetchFilm} from '../../store/api-actions';
 import {ActionCreator} from '../../store/actions';
-import {getRuntime} from '../../utils';
 
 import VideoPlayerFull from './video-player-full/video-player-full';
 import LoadingScreen from '../loading-screen/loading-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
+import VideoPlayerControls from './video-player-controls/video-player-controls';
+
 
 const PlayerScreen = ({id, film, isLoadFilm, isLoadFilmFailed, onLoadFilm}) => {
   const history = useHistory();
+  const [isPlaying, setPlaying] = useState(true);
+  const [isReverse, setReverse] = useState(false);
+  const [isFullScreen, setFullScreen] = useState(false);
+  const [videoTime, setVideoTime] = useState({
+    progress: 0,
+    duration: 0,
+  });
 
   useEffect(() => {
     if (!isLoadFilm) {
@@ -24,51 +32,63 @@ const PlayerScreen = ({id, film, isLoadFilm, isLoadFilmFailed, onLoadFilm}) => {
     }
   }, [isLoadFilm]);
 
+
   if (!isLoadFilm && !isLoadFilmFailed) {
     return <LoadingScreen />;
-  } if (!isLoadFilm && isLoadFilmFailed) {
+  }
+
+  if (!isLoadFilm && isLoadFilmFailed) {
     return <NotFoundScreen />;
   }
 
-  const {name, previewImage, videoLink, runTime} = film;
+
+  const {previewImage, videoLink} = film;
+
+  const handlerPlayerTimeClick = () => {
+    setReverse(!isReverse);
+  };
+
+  const handlerPlayClick = () => {
+    setPlaying(!isPlaying);
+  };
+
+  const handlerFullScreenClick = () => {
+    setFullScreen(true);
+  };
+
+  const handlerExitClick = () => {
+    history.push(`/films/${id}`);
+  };
 
   return (
     <div className="player">
 
-      <VideoPlayerFull videoLink={videoLink} previewImage={previewImage} />
+      <VideoPlayerFull
+        videoLink={videoLink}
+        previewImage={previewImage}
+        isPlaying={isPlaying}
+        isFullScreen={isFullScreen}
+        onPlaying={setPlaying}
+        onFullScreen={setFullScreen}
+        onVideoTime={setVideoTime}
+      />
 
       <button
         type="button"
         className="player__exit"
-        onClick={() => history.push(`/films/${id}`)}
+        onClick={handlerExitClick}
       >Exit</button>
 
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style={{left: `30%`}}>Toggler</div>
-          </div>
-          <div className="player__time-value">{getRuntime(runTime)}</div>
-        </div>
+      <VideoPlayerControls
+        nameFilm={film.name}
+        videoTime={videoTime}
+        isReverse={isReverse}
+        isPlaying={isPlaying}
+        handlerPlayerTimeClick={handlerPlayerTimeClick}
+        handlerPlayClick={handlerPlayClick}
+        handlerFullScreenClick={handlerFullScreenClick}
+      />
 
-        <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <div className="player__name">{name}</div>
-
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
-        </div>
-      </div>
     </div>
   );
 };

357d51876f6ff8560a3ea7e343265149be397c0c
vovapipko <vpmail13@gmail.com>
2021-03-29 22:29:59 +0300

    Разобъёт редьюсер по типам действий WIP

---
+++ src/store/user/user.js
@@ -0,0 +1,17 @@
+import {ActionTypes} from '../types';
+import {AuthorizationStatus} from '../../const';
+
+const initialState = {
+  user: {},
+  authorizationStatus: AuthorizationStatus.NO_AUTH,
+};
+
+export const user = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.LOAD_USER:
+      return {...state, user: action.payload};
+    case ActionTypes.REQUIRED_AUTHORIZATION:
+      return {...state, authorizationStatus: action.payload};
+    default: return state;
+  }
+};
